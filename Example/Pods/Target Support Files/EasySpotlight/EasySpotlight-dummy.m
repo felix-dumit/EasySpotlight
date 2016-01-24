6396b82077729c031fd802d75fb4b7600dab7724
Felix Dumit <felix.dumit@gmail.com>
2016-01-23 17:24:36 -0800

    remove pods folder

--- Example/Pods/Target Support Files/EasySpotlight/EasySpotlight-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_EasySpotlight : NSObject
-@end
-@implementation PodsDummy_EasySpotlight
-@end
--- Example/Pods/Target Support Files/Pods-EasySpotlight_Tests/Pods-EasySpotlight_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_EasySpotlight_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_EasySpotlight_TestsVersionString[];
-
